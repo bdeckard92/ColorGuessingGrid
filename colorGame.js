e6d21eee5fbc862024f003b5a145b527d7bd1313
Bob Deckard <bdeckard92@gmail.com>
2017-06-16 15:13:15 -0500

    with an actual repository

---
+++ colorGame.js
@@ -0,0 +1,122 @@
+console.log(5 + 6);
+var numSquares = 6;
+var colors = generateRandomColors(numSquares);
+var squares = document.querySelectorAll(".square");
+var pickedColor = pickColor();
+var colorDisplay = document.getElementById("colorDisplay");
+var messageDisplay = document.querySelector("#message");
+var h1 = document.querySelector("h1");
+var resetButton = document.querySelector("#reset");
+var easyBtn = document.querySelector("#easyBtn");
+var hardBtn = document.querySelector("#hardBtn");
+var h2 = document.querySelector("h2");
+var title = document.querySelector(".title");
+var modeButtons = document.querySelectorAll(".mode");
+
+for (var i = 0; i < modeButtons.length; i++) {
+    modeButtons[i].addEventListener("click", function() {
+        modeButtons[0].classList.remove("selected");
+        modeButtons[1].classList.remove("selected");
+        this.classList.add("selected");
+        // terinary operator
+        this.textContent === "Easy" ? numSquares = 3: numSquares = 6;
+        //the above replaces and does the same thing as the if else statement commented below
+        /*if (this.textContent === "Easy") {
+            numSquares = 3;
+        }else {
+        	numSquares = 6;
+        }*/
+        reset();
+    });
+}
+
+function reset() {
+    colors = generateRandomColors(numSquares);
+    pickedColor = pickColor();
+    colorDisplay.textContent = pickedColor;
+    resetButton.textContent = "New Colors";
+    messageDisplay.textContent = "";
+    for (var i = 0; i < squares.length; i++) {
+    	if(colors[i]){
+    		squares[i].style.display = "block";
+    		squares[i].style.backgroundColor = colors[i];
+    	} else {
+    		squares[i].style.display = "none";
+    	}
+        
+    }
+    h1.style.backgroundColor = "steelblue";
+    h2.style.backgroundColor = "steelblue";
+    title.style.backgroundColor = "steelblue";
+
+}
+
+
+resetButton.addEventListener("click", function() {
+    colors = generateRandomColors(numSquares);
+    pickedColor = pickColor();
+    colorDisplay.textContent = pickedColor;
+    this.textContent = "New Colors";
+    for (var i = 0; i < squares.length; i++) {
+        squares[i].style.backgroundColor = colors[i];
+    }
+    h1.style.backgroundColor = "steelblue";
+    h2.style.backgroundColor = "steelblue";
+    title.style.backgroundColor = "steelblue";
+    messageDisplay.textContent = "";
+});
+
+colorDisplay.textContent = pickedColor;
+
+for (var i = 0; i < squares.length; i++) {
+    //add initial color to squares
+    squares[i].style.backgroundColor = colors[i];
+    //add event listener
+    squares[i].addEventListener("click", function() {
+        //grab color of clicked square
+        var clickedColor = this.style.backgroundColor;
+        if (clickedColor === pickedColor) {
+            messageDisplay.textContent = "Correct";
+            changeColors(clickedColor);
+            h1.style.backgroundColor = clickedColor;
+            h2.style.backgroundColor = clickedColor;
+            title.style.backgroundColor = clickedColor;
+            resetButton.textContent = "Play Again?"
+        } else {
+            this.style.backgroundColor = "#232323";
+            messageDisplay.textContent = "Try Again";
+        }
+    });
+}
+
+function changeColors(color) {
+    for (var i = 0; i < squares.length; i++) {
+        squares[i].style.backgroundColor = color;
+    }
+}
+
+function pickColor() {
+    var random = Math.floor(Math.random() * colors.length);
+    return colors[random];
+}
+
+function generateRandomColors(num) {
+    //make array
+    var arr = []
+        //add num random colors to array
+    for (var i = 0; i < num; i++) {
+        //get random color and push into arr
+        arr.push(randomColor())
+    }
+    //return array
+    return arr;
+}
+
+function randomColor() {
+    //pick red from 0 - 255 and repeat for each color
+    var r = Math.floor(Math.random() * 256);
+    var g = Math.floor(Math.random() * 256);
+    var b = Math.floor(Math.random() * 256);
+
+    return "rgb(" + r + ", " + g + ", " + b + ")";
+}
